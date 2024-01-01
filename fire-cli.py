b7025221806ebe044c6cb758d6eec968d39bee1d
yangxu21 <155080574+yangxu21@users.noreply.github.com>
2024-01-01 22:18:40 +0000

    adding fire

---
+++ fire-cli.py
@@ -0,0 +1,7 @@
+import fire
+from mylib.bot import scrape
+
+if __name__=="__main__":
+    fire.Fire(scrape)  # a super easy way to make command line tool (instead of using click)
+                       # python fire-cli.py --name Facebook --length 2
+
